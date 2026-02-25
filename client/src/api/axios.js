7f373870d8cf7c6e0e1c49410a3927de49d6856d
Sudhir Kumar <sk2001may@gmail.com>
2026-02-25 16:36:55 +0530

    missing fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:5000/api"
-  baseURL:"https://rag-bot-back.onrender.com",
+  // baseURL: "http://localhost:5000/api",
+  baseURL:"https://rag-bot-back.onrender.com/api",
   withCredentials: true, // ✅ cookie send karega
 });
 
