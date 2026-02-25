a5b9142b771dbdd848351da946cbb0d793ae31fe
Sudhir Kumar <sk2001may@gmail.com>
2026-02-25 16:21:10 +0530

    production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api"
+  baseURL:"https://rag-bot-llm.onrender.com",
   withCredentials: true, // ✅ cookie send karega
 });
 
